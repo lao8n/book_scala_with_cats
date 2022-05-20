c7847476f7cee3509f40325326497b2b0e3b6a79
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2022-05-20 18:05:42 +0100

    ex 1.3 passing and working

---
+++ src/main/scala/sandbox/Ex1p3.scala
@@ -0,0 +1,43 @@
+package sandbox
+
+import PrintableInstances._
+
+trait Printable[A] {
+  def format(value : A): String
+}
+
+object PrintableInstances {
+  implicit val pString : Printable[String] =
+    new Printable[String] {
+      def format(value: String): String =
+        value
+    }
+
+  implicit val pInt : Printable[Int] =
+    new Printable[Int] {
+      def format(value: Int): String =
+        value.toString()
+    }
+}
+
+object Printable {
+  def format[A](value : A)(implicit p: Printable[A]): String = {
+    p.format(value)
+  }
+  def print[A](value: A)(implicit p: Printable[A]): Unit = {
+    println(format(value))
+  }
+}
+
+final case class Cat(name: String, age: Int, color: String)
+
+object Cat {
+  implicit val catPrintable = new Printable[Cat] {
+    def format(cat: Cat): String = {
+      val name  = Printable.format(cat.name)
+      val age   = Printable.format(cat.age)
+      val color = Printable.format(cat.color)
+      s"$name is a $age year-old $color cat."
+    }
+  }
+}
--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -5,4 +5,8 @@ import cats.syntax.semigroup._
 
 object Main extends App {
   println("Hello " |+| "Cats!")
+
+  // Exercise 1.3 https://www.scalawithcats.com/dist/scala-with-cats.html#exercise-printable-library
+  val cat = Cat("elon", 5, "black")
+  Printable.print(cat)
 }
