5d4a61fd53098619473813cf77253f30a1b84ec6
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2022-06-17 14:11:42 +0100

    do ex1p5p5

---
+++ src/main/scala/sandbox/Ex1p5p5.scala
@@ -0,0 +1,17 @@
+package sandbox
+
+import cats.Eq
+import cats.instances.option._
+import cats.instances.int._ // for int eq
+import cats.instances.string._ // for string eq
+import cats.syntax.eq._ // for === and =!=
+
+final case class CatEq(name: String, age: Int, color: String)
+
+object CatEq {
+  implicit val catEq: Eq[CatEq] =
+    Eq.instance[CatEq] {
+      (cat1, cat2) =>
+        cat1.age === cat2.age & cat1.name === cat2.name & cat1.color === cat2.name
+    }
+}
--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -3,15 +3,27 @@ package sandbox
 import cats.implicits.toShow
 import cats.instances.string._
 import cats.syntax.semigroup._
+import cats.syntax.eq._ // for === and =!=
+import cats.instances.option._
 
 object Main extends App {
   println("Hello " |+| "Cats!")
 
   // Exercise 1.3 https://www.scalawithcats.com/dist/scala-with-cats.html#exercise-printable-library
-  val cat = Cat("elon", 5, "black")
-  Printable.print(cat)
+  val cat1 = Cat("elon", 5, "black")
+  Printable.print(cat1)
 
   // Exercise 1.4.6 https://www.scalawithcats.com/dist/scala-with-cats.html#exercise-cat-show
-  val cat1 = CatShow("elon", 5, "black")
-  println(cat1.show)
+  val cat2 = CatShow("elon", 5, "white")
+  println(cat2.show)
+
+  // Exercise 1.5.5 https://www.scalawithcats.com/dist/scala-with-cats.html#exercise-equality-liberty-and-felinity
+  val cat3 = CatEq("Garfield", 38, "orange and black")
+  val cat4 = CatEq("Heathcliff", 33, "orange and black")
+  val optionCat3 = Option(cat3)
+  val optionCat4 = Option.empty[CatEq]
+  println(cat3 === cat4)
+  println(cat3 =!= cat4)
+  println(optionCat3 === optionCat4)
+  println(optionCat3 =!= optionCat4)
 }
